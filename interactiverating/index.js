291a92f7635570083dd3ecbbd7355e0f7f6f2b6d
KnottBound <danielgknott@gmail.com>
2023-11-01 14:14:43 -0400

    Added Desktop CSS + JS Working Now
    - Fixed JS to work
    - Added Desktop CSS

--- interactiverating/index.js
+++ interactiverating/index.js
@@ -1,15 +1,18 @@
 function showThankYouState() {
-    //Hide One and Show Other
-    document.getElementById("ratingState").style.display = "none";
-    document.getElementById("thankYouState").style.display = "block";
-}
+    const thankYouState = document.getElementById("thankYouState");
+    if (thankYouState) {
+      document.getElementById("ratingState").style.display = "none";
+      thankYouState.style.display = "flex"; // Show the element
+      thankYouState.classList.add("submitted"); // Apply the styles
+    }
+  }
 
-document.getElementById("myBtn").addEventListener("click", function(event) {
+
+  document.getElementById("myBtn").addEventListener("click", function(event) {
     event.preventDefault();
 
     showThankYouState();
-});
-
+  });
 
 function ratingSelect(value) {
     //Stores the selected value
