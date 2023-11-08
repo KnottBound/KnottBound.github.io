7c747b47ff8b767c0ebac719eda4ca6f26262a13
KnottBound <danielgknott@gmail.com>
2023-11-07 22:40:10 -0500

    -Mobile Nav Bar Formatting
    - Nav Bar Formatting

--- intronav/index.js
+++ intronav/index.js
@@ -1,10 +1,18 @@
 
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
+const menu_close = document.querySelector('.openhamburger')
 
 menu_btn.addEventListener("click", function() {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
+    menu_close.classList.toggle('is-active');
+})
+
+menu_close.addEventListener("click", function() {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+    menu_close.classList.toggle('is-active');
 })
 
 
